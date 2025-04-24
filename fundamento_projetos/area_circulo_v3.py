155d4c3ce281eef4948a9e2b4d1942447a8c35d3
Junior <juniorn06@gmail.com>
2025-04-24 01:06:07 -0300

    area_circulo versão 4

---
+++ fundamento_projetos/area_circulo_v3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#import math
+from math import pi
+
+raio = 15.3
+area = pi * pow(raio, 2)
+
+print('Área do círculo:', area)
---
+++ fundamento_projetos/area_circulo_v4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#import math
+from math import pi
+
+raio = input('Informe o raio: ')
+area = pi * pow(float(raio), 2)
+
+print('Área do círculo:', area)
